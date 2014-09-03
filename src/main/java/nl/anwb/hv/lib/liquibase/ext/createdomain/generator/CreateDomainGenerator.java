8464ecb68095f562a6d18a02ccf1d1836dc30766
Hans Bogaards <hbogaards@anwb.nl>
2014-09-03 11:23:29 +0200

    Fixes to the CreateDomainGenerator

--- src/main/java/nl/anwb/hv/lib/liquibase/ext/createdomain/generator/CreateDomainGenerator.java
+++ src/main/java/nl/anwb/hv/lib/liquibase/ext/createdomain/generator/CreateDomainGenerator.java
@@ -3,7 +3,6 @@ package nl.anwb.hv.lib.liquibase.ext.createdomain.generator;
 import java.util.ArrayList;
 import java.util.List;
 
-import nl.anwb.hv.lib.liquibase.ext.createdomain.statement.CreateDomainStatement;
 import liquibase.database.Database;
 import liquibase.database.core.SybaseDatabase;
 import liquibase.database.typeconversion.TypeConverterFactory;
@@ -12,6 +11,8 @@ import liquibase.sql.Sql;
 import liquibase.sql.UnparsedSql;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.sqlgenerator.core.AbstractSqlGenerator;
+
+import nl.anwb.hv.lib.liquibase.ext.createdomain.statement.CreateDomainStatement;
 
 public class CreateDomainGenerator extends AbstractSqlGenerator<CreateDomainStatement> {
 
@@ -31,13 +32,13 @@ public class CreateDomainGenerator extends AbstractSqlGenerator<CreateDomainStat
 
     @Override
     public Sql[] generateSql(CreateDomainStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-        
+
         if (database instanceof SybaseDatabase) {
             return generateSqlSybase(statement, database, sqlGeneratorChain);
         }
-        
+
         StringBuilder createBuilder = new StringBuilder();
-        
+
         createBuilder.append("CREATE DOMAIN ");
         if (statement.isIfNotExists()) {
             createBuilder.append("IF NOT EXISTS ");
@@ -45,8 +46,8 @@ public class CreateDomainGenerator extends AbstractSqlGenerator<CreateDomainStat
         createBuilder.append(database.escapeDatabaseObject(statement.getDomainName()));
         createBuilder.append(" AS ");
         createBuilder.append(TypeConverterFactory.getInstance().findTypeConverter(database).getDataType(statement.getDataType(), false));
-        
-        Object defaultValue = statement.getDefaultValue(); 
+
+        Object defaultValue = statement.getDefaultValue();
         if (defaultValue != null) {
             createBuilder.append(" DEFAULT ");
             createBuilder.append(TypeConverterFactory.getInstance().findTypeConverter(database).getDataType(defaultValue).convertObjectToString(defaultValue, database));
@@ -60,62 +61,62 @@ public class CreateDomainGenerator extends AbstractSqlGenerator<CreateDomainStat
             createBuilder.append(check);
             createBuilder.append(")");
         }
-        return new Sql[] 
+        return new Sql[]
             {
                 new UnparsedSql(createBuilder.toString())
             };
     }
-    
+
     private Sql[] generateSqlSybase(CreateDomainStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
         List<Sql> sql = new ArrayList<Sql>();
-      
+
         boolean hasCheck = false;
         boolean hasDefault = false;
-        
+
         if (statement.getCheck() != null && !statement.getCheck().isEmpty()) {
             hasCheck = true;
-            
+
             StringBuilder ruleBuilder = new StringBuilder();
-            
+
             ruleBuilder.append("CREATE RULE ");
             ruleBuilder.append("rl_");
             ruleBuilder.append(statement.getDomainName());
             ruleBuilder.append(" AS ");
-            
+
             String rule = statement.getCheck().replaceAll("value", "@value");
             ruleBuilder.append(rule);
-            
+
             sql.add(new UnparsedSql(ruleBuilder.toString()));
         }
-        
+
         if (statement.getDefaultValue() != null) {
             hasDefault = true;
-            Object defaultValue = statement.getDefaultValue(); 
-            
+            Object defaultValue = statement.getDefaultValue();
+
             StringBuilder defaultBuilder = new StringBuilder();
-            
+
             defaultBuilder.append("CREATE DEFAULT ");
-            defaultBuilder.append("dlft_");
+            defaultBuilder.append("dflt_");
             defaultBuilder.append(statement.getDomainName());
             defaultBuilder.append(" AS ");
             defaultBuilder.append(TypeConverterFactory.getInstance().findTypeConverter(database).getDataType(defaultValue).convertObjectToString(defaultValue, database));
-            
+
             sql.add(new UnparsedSql(defaultBuilder.toString()));
         }
         StringBuilder createBuilder = new StringBuilder();
-        
+
         createBuilder.append("exec sp_addtype '");
         createBuilder.append(statement.getDomainName());
         createBuilder.append("', '");
         createBuilder.append(statement.getDataType());
         createBuilder.append("', ");
         createBuilder.append(statement.isNullable() ? "null" : "nonull");
-        
+
         sql.add(new UnparsedSql(createBuilder.toString()));
-        
+
         if (hasCheck) {
             StringBuilder bindBuilder = new StringBuilder();
-            
+
             bindBuilder.append("sp_bindrule '");
             bindBuilder.append(statement.getSchemaName());
             bindBuilder.append(".rl_");
@@ -128,7 +129,7 @@ public class CreateDomainGenerator extends AbstractSqlGenerator<CreateDomainStat
         }
         if (hasDefault) {
             StringBuilder bindBuilder = new StringBuilder();
-            
+
             bindBuilder.append("sp_bindefault '");
             bindBuilder.append(statement.getSchemaName());
             bindBuilder.append(".dflt_");
@@ -139,7 +140,7 @@ public class CreateDomainGenerator extends AbstractSqlGenerator<CreateDomainStat
 
             sql.add(new UnparsedSql(bindBuilder.toString()));
         }
-        
+
         return sql.toArray(new Sql[0]);
     }
 }
